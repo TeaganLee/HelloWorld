70f526b93d8941fa13db53b43a48a305025eb80a
Teagan Lee <teagan.lee.2020@student.lhschools.org>
2020-02-24 12:44:01 -0500

    IntelliJ Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,13 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("My name is ");
+        System.out.println("Teagan.");
+        System.out.print("During my summer, I spent a week and a half with my grandparents in Colorado. ");
+        System.out.print("I also did a lot of work at Wesley Woods, and participated in some college visits. ");
+        System.out.println("Can't wait to get to know you too!");
+    }
+}
